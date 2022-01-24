0e931582856114df17dd78e6d25ec879020d1a21
leesjensen <leesjensen@gmail.com>
2022-01-24 06:17:34 -0700

    enhance: C4 animate bucket list

--- www/css/css-c4/main.js
+++ www/css/css-c4/main.js
@@ -2,4 +2,7 @@ function toggleBucketList() {
   const bucketList = document.getElementById('bucket-list');
   const displayed = bucketList.style.display === 'block';
   bucketList.style.display = displayed ? 'none' : 'block';
+  if (bucketList.style.display == 'block') {
+    bucketList.style.animation = `slideIn .5s`;
+  }
 }
