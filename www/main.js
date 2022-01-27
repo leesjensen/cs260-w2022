894b09f5b1139e35131252d2cc5ece014a063b82
Lee <lee.jensen@agilix.com>
2022-01-26 18:48:31 -0700

    Made main page have collapsable unit boxes.

--- www/main.js
+++ www/main.js
@@ -2,10 +2,12 @@ function toggleDisplay(e) {
   const expandableElement = e.parentElement.querySelector(".expandable");
   console.log(expandableElement);
   if (expandableElement) {
-    if (expandableElement.classList.contains("expanded")) {
-      expandableElement.classList.remove("expanded");
+    if (expandableElement.classList.contains("collapsed")) {
+      expandableElement.classList.remove("collapsed");
+      expandableElement.parentElement.classList.remove("collapsed");
     } else {
-      expandableElement.classList.add("expanded");
+      expandableElement.classList.add("collapsed");
+      expandableElement.parentElement.classList.add("collapsed");
     }
   }
 }
