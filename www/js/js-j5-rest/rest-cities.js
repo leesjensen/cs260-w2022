dccee8be4ab497b91216907363c2e7d4e4797298
Lee <leesjensen@gmail.com>
2022-01-31 21:32:29 -0700

    fix: j5 cities defaults to .* regex because cs260.click/api/city takes a regex

--- www/js/js-j5-rest/rest-cities.js
+++ www/js/js-j5-rest/rest-cities.js
@@ -1,31 +1,31 @@
 function cities() {
-  let query = document.getElementById("cityField").value;
+  let query = document.getElementById('cityField').value;
   query = normalizeQuery(query);
 
-  const url = "https://cs260.click/api/city?q=" + query;
+  const url = 'https://cs260.click/api/city?q=' + query;
   fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      var html = "";
+      var html = '';
       for (let i = 0; i < json.length; i++) {
         html += `<li>${json[i].city}</li>`;
       }
-      document.getElementById("output").innerHTML = `<ul>${html}</ul>`;
+      document.getElementById('output').innerHTML = `<ul>${html}</ul>`;
     })
     .catch((error) => {
-      document.getElementById("output").innerHTML = error;
+      document.getElementById('output').innerHTML = error;
     });
 }
 
 function normalizeQuery(query) {
   if (query) {
-    const queryParts = query.split(" ");
+    const queryParts = query.split(' ');
     for (let i = 0; i < queryParts.length; i++) {
       queryParts[i] =
         queryParts[i].charAt(0).toUpperCase() + queryParts[i].slice(1);
     }
 
-    return queryParts.join(" ");
+    return queryParts.join(' ');
   }
-  return "*";
+  return '.*';
 }
