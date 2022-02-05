fc28ed51944165687bec477dd4e046a70f5824b8
leesjensen <leesjensen@gmail.com>
2022-02-05 05:58:25 -0700

    new: dom demo

---
+++ www/js/js-dom-manipulation/dom.js
@@ -0,0 +1,24 @@
+function injectDOMOperations(title) {
+  const parentElement = document.getElementById('output');
+
+  while (parentElement.firstChild) {
+    parentElement.removeChild(parentElement.firstChild);
+  }
+
+  const newElement = document.createElement('h3');
+  newElement.innerText = title;
+  parentElement.appendChild(newElement);
+}
+
+function injectDOMHTML(title) {
+  const parentElement = document.getElementById('output');
+
+  parentElement.innerHTML = `<h3>${title}</h3>`;
+}
+
+function injectHTMLFromInput(title) {
+  const inputElement = document.getElementById('html-text');
+  const parentElement = document.getElementById('output');
+
+  parentElement.innerHTML = inputElement;
+}
--- www/js/js-j5-table/table.js
+++ www/js/js-j5-table/table.js
@@ -3,23 +3,31 @@
 // Right align cells that contain number values (text-align:right)
 
 const SevenSummits = [
-  { name: "Everest", height: 8848, place: "Nepal" },
-  { name: "Aconcagua", height: 6961, place: "Argentina" },
-  { name: "Denali", height: 6194, place: "United States" },
-  { name: "Kilimanjaro", height: 5895, place: "Tanzania" },
-  { name: "Elbrus", height: 5642, place: "Russia" },
-  { name: "Vinson", height: 4892, place: "Antarctica" },
-  { name: "Puncak Jaya", height: 4884, place: "Indonesia" },
+  { name: 'Everest', height: 8848, place: 'Nepal' },
+  { name: 'Aconcagua', height: 6961, place: 'Argentina' },
+  { name: 'Denali', height: 6194, place: 'United States' },
+  { name: 'Kilimanjaro', height: 5895, place: 'Tanzania' },
+  { name: 'Elbrus', height: 5642, place: 'Russia' },
+  { name: 'Vinson', height: 4892, place: 'Antarctica' },
+  { name: 'Puncak Jaya', height: 4884, place: 'Indonesia' },
 ];
 
-const UtahCountySevenSummits = [
-  { name: "Timpanogos", height: 11750, stars: 4.8 },
-  { name: "Santaquin", height: 10687, stars: 3.8 },
-  { name: "Lone Peak", height: 11253, stars: 5 },
-  { name: "Provo Peak", height: 11068, stars: 4.1 },
-  { name: "Cascade", height: 10908, stars: 3.2 },
-  { name: "Nebo", height: 11928, stars: 4.8 },
-  { name: "Spanish Fork", height: 10192, stars: 3.4 },
+const UtahCountySevenPeaks = [
+  { name: 'Timpanogos', height: 11750, stars: 4.8 },
+  { name: 'Santaquin', height: 10687, stars: 3.8 },
+  { name: 'Lone Peak', height: 11253, stars: 5 },
+  { name: 'Provo Peak', height: 11068, stars: 4.1 },
+  { name: 'Cascade', height: 10908, stars: 3.2 },
+  { name: 'Nebo', height: 11928, stars: 4.8 },
+  { name: 'Spanish Fork', height: 10192, stars: 3.4 },
+];
+
+const JazzMusic = [
+  { title: 'Take Five', artist: 'Dave Brubeck', stars: 4.8 },
+  { title: 'So What', artist: 'Miles Davis', stars: 3.8 },
+  { title: 'Take The A Train', artist: 'Duke Ellington', stars: 4.2 },
+  { title: 'Round Midnight', artist: 'Thelonious Monk', stars: 3.1 },
+  { title: 'My Favorite Things', artist: 'John Coltrane', stars: 3.0 },
 ];
 
 let currentData = SevenSummits;
@@ -31,14 +39,14 @@ function table(data = SevenSummits) {
     const headers = parseHeader(data);
     const tableElement = generateTable(headers, data);
 
-    const output = document.getElementById("output");
+    const output = document.getElementById('output');
 
     removeAllChildNodes(output);
     output.appendChild(tableElement);
 
     // outputData("parse", { headers: headers, rows: rows });
   } else {
-    outputData("invalid input", data);
+    outputData('invalid input', data);
   }
 }
 
@@ -51,8 +59,8 @@ function parseHeader(data) {
 }
 
 function generateTable(headers, data) {
-  const tableElement = document.createElement("table");
-  tableElement.classList.add("a");
+  const tableElement = document.createElement('table');
+  tableElement.classList.add('a');
 
   addTableStyles(headers);
 
@@ -63,13 +71,13 @@ function generateTable(headers, data) {
 }
 
 function generateHeader(headers, tableElement) {
-  const rowElement = document.createElement("tr");
+  const rowElement = document.createElement('tr');
   tableElement.appendChild(rowElement);
 
   headers.forEach((header) => {
-    const cellElement = document.createElement("th");
+    const cellElement = document.createElement('th');
     rowElement.appendChild(cellElement);
-    cellElement.setAttribute("onclick", `sortColumn(this)`);
+    cellElement.setAttribute('onclick', `sortColumn(this)`);
     const textNode = document.createTextNode(header.name);
     cellElement.appendChild(textNode);
   });
@@ -77,10 +85,10 @@ function generateHeader(headers, tableElement) {
 
 function generateRows(data, tableElement) {
   data.forEach((dataRow) => {
-    const rowElement = document.createElement("tr");
+    const rowElement = document.createElement('tr');
     tableElement.appendChild(rowElement);
     for (const [, value] of Object.entries(dataRow)) {
-      const cellElement = document.createElement("td");
+      const cellElement = document.createElement('td');
       rowElement.appendChild(cellElement);
       const textNode = document.createTextNode(value);
       cellElement.appendChild(textNode);
@@ -89,11 +97,11 @@ function generateRows(data, tableElement) {
 }
 
 function addTableStyles(headers) {
-  insertRule("#output table {border-collapse: collapse;}");
-  insertRule("#output th,td {border: solid white thin;padding:.25em;}");
-  insertRule(".selected {background: white; color:black;}");
+  insertRule('#output table {border-collapse: collapse;}');
+  insertRule('#output th,td {border: solid white thin;padding:.25em;}');
+  insertRule('.selected {background: white; color:black;}');
   headers.forEach((header, index) => {
-    if (header.type === "number") {
+    if (header.type === 'number') {
       insertRule(`#output tr td:nth-child(${index + 1}) {text-align:right;}`);
     }
   });
@@ -107,9 +115,7 @@ function insertRule(rule) {
 function sortColumn(column) {
   sortDirection *= -1;
   const sortBy = column.innerText;
-  const sortedData = currentData.sort(
-    (a, b) => sortDirection * (a[sortBy] > b[sortBy] ? 1 : -1)
-  );
+  const sortedData = currentData.sort((a, b) => sortDirection * (a[sortBy] > b[sortBy] ? 1 : -1));
   table(sortedData);
 }
 
@@ -120,10 +126,6 @@ function removeAllChildNodes(parent) {
 }
 
 function outputData(title, data) {
-  const output = document.getElementById("output");
-  output.innerHTML = `<h3>${title}</h3><pre>${JSON.stringify(
-    data,
-    null,
-    2
-  )}</pre>`;
+  const output = document.getElementById('output');
+  output.innerHTML = `<h3>${title}</h3><pre>${JSON.stringify(data, null, 2)}</pre>`;
 }
