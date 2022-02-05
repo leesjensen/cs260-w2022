370c6b9b7bf1659b3c9e2aa6bd768dd22588ac04
leesjensen <leesjensen@gmail.com>
2022-02-05 07:00:04 -0700

    enhance: moved dom hack examples to HTML

--- www/js/js-dom-tutorial/dom.js
+++ www/js/js-dom-tutorial/dom.js
@@ -16,12 +16,6 @@ function injectDOMHTML(title) {
   parentElement.innerHTML = `<h3>${title}</h3>`;
 }
 
-// <b style="color:red">hello</b>
-// <script>alert("danger")</script>
-// <img src='x' onerror='alert("danger")'/>
-// <img src='x' onerror="fetch('https://cs260.click/hacked.json').then(r=>r.json()).then(j=>console.log(j))"/>
-// <img src='x' onerror="fetch('https://cs260.click/hacked.json').then(r=>r.json()).then(j=>document.getElementById('output').innerHTML =j.msg)"/>
-
 function injectHTMLFromInput() {
   const inputElement = document.getElementById('html-text');
   const parentElement = document.getElementById('output');
