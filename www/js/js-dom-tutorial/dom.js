deea11384ed330c5cd5c4fc7e90ff9ddcb88cdc3
leesjensen <leesjensen@gmail.com>
2022-02-05 07:14:42 -0700

    cleanup: dom tutorial

--- www/js/js-dom-tutorial/dom.js
+++ www/js/js-dom-tutorial/dom.js
@@ -1,3 +1,9 @@
+function injectDOMHTML(title) {
+  const parentElement = document.querySelector('#output');
+
+  parentElement.innerHTML = `<h3>${title}</h3>`;
+}
+
 function injectDOMOperations(title) {
   const parentElement = document.getElementById('output');
 
@@ -8,12 +14,6 @@ function injectDOMOperations(title) {
   const newElement = document.createElement('h3');
   newElement.innerText = title;
   parentElement.appendChild(newElement);
-}
-
-function injectDOMHTML(title) {
-  const parentElement = document.getElementById('output');
-
-  parentElement.innerHTML = `<h3>${title}</h3>`;
 }
 
 function injectHTMLFromInput() {
