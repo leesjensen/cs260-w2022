cf032cebdf3af0df8408c89a1b5cb6e9a69c7629
Lee <lee.jensen@agilix.com>
2022-02-07 12:05:04 -0700

    cleanup: promises demo

--- www/js/js-j4-promises/promises.js
+++ www/js/js-j4-promises/promises.js
@@ -4,84 +4,75 @@ async function pickupPizza() {
   const order = createOrder();
 
   // Promise
-  // placeOrder(order)
-  //   .then((time) => {
-  //     order.cashierTime = time;
-  //     return makePizza(order);
-  //   })
-  //   .then((time) => {
-  //     order.cookTime = time;
-  //     return serveOrder(order);
-  //   })
-  //   .catch((err) => {
-  //     order.error = err;
-  //     orderFailure(order);
-  //   });
+  placeOrder(order)
+    .then((order) => makePizza(order))
+    .then((order) => serveOrder(order))
+    .catch((order) => {
+      orderFailure(order);
+    });
 
   // async/await
   // try {
-  //   order.cashierTime = await placeOrder();
-  //   order.cookTime = await makePizza();
+  //   await placeOrder(order);
+  //   await makePizza(order);
   //   serveOrder(order);
-  // } catch (err) {
-  //   order.error = err;
+  // } catch (order) {
   //   orderFailure(order);
   // }
 
   // Consider this alteration
-  try {
-    const place = placeOrder(order.id, order);
-    const make = makePizza(order.id, order);
-    order.cashierTime = await place;
-    order.cookTime = await make;
-    serveOrder(order);
-  } catch (err) {
-    console.log(`Error order #${order.id}, ${err}`);
-    order.error = err;
-    orderFailure(order);
-  }
+  // try {
+  //   const place = placeOrder(order);
+  //   const make = makePizza(order);
+  //   await place;
+  //   await make;
+  //   serveOrder(order);
+  // } catch (order) {
+  //   orderFailure(order);
+  // }
 }
 
 function createOrder() {
+  // Make the order and associate it with a new HTML element
   const id = Math.floor(Math.random() * 10000);
   const orderElement = document.createElement('li');
+  const order = { element: orderElement, id: id };
+
+  // Insert the order into the HTML list
   orderElement.innerHTML = `<span>&#128523; <i>Waiting</i> [${id}] ...</span>`;
   const orders = document.getElementById('orders');
   orders.appendChild(orderElement);
-  return { element: orderElement, id: id };
+
+  return order;
 }
 
-function placeOrder(id, order) {
+function placeOrder(order) {
   return new Promise((resolve, reject) => {
-    doWork(1000, 3000, resolve, reject, `cashier too busy ${id} #${order.id}`);
+    doWork(order, 1000, 3000, resolve, reject, `cashier too busy`);
   });
 }
 
-function makePizza(id, order) {
+function makePizza(order) {
   return new Promise((resolve, reject) => {
-    doWork(2000, 5000, resolve, reject, `cook burnt pizza ${id} #${order.id}`);
+    doWork(order, 2000, 5000, resolve, reject, `cook burnt pizza`);
   });
 }
 
-function doWork(min, max, resolve, reject, errMsg) {
+function doWork(order, min, max, resolve, reject, errMsg) {
   let workTime = Math.random() * (max - min) + min;
   setTimeout(() => {
     workTime = Math.round(workTime);
     if (workTime < max * 0.85) {
-      resolve(workTime);
+      resolve(order);
     } else {
-      reject(errMsg);
+      order.error = errMsg;
+      reject(order);
     }
   }, workTime);
 }
 
 function serveOrder(order) {
-  order.element.innerHTML = `
-   <span>&#127829; <b>Served</b>!
-      [${order.id}] 
-      cashier: ${Math.floor(order.cashierTime / 1000)}s,
-      cook: ${Math.floor(order.cookTime / 1000)}s
-   </span>`;
+  order.element.innerHTML = `<span>&#127829; <b>Served</b>! [${order.id}]</span>`;
 }
 
 function orderFailure(order) {
