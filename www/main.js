cb9f931be4ff53d8fb88ef6c836ba322028a978f
Lee <lee.jensen@agilix.com>
2022-02-07 07:48:57 -0700

    style: main page

--- www/main.js
+++ www/main.js
@@ -25,12 +25,12 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 1.1 - Introduction',
+        title: '1.1 - Introduction',
         link: 'https://docs.google.com/presentation/d/1Y7J2fmn9VjY8NF9lSPQgLVbVn1qQR4DCzu57lUOCclw/edit?usp=sharing',
       },
       {
         class: 'slide',
-        title: '&#127910; 1.2 - Server Setup',
+        title: '1.2 - Server Setup',
         link: 'https://docs.google.com/presentation/d/1S5nLhokdBJeBUSW051a_BtwMKD7UA6w-vtvNrSw7qjU/edit?usp=sharing',
       },
       {
@@ -40,7 +40,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 1.3 - Editors &amp; HTML',
+        title: '1.3 - Editors &amp; HTML',
         link: 'https://docs.google.com/presentation/d/1fdRNqK6dlWi08_88BnVjYAdYWNfqwGhrQrv6GE7TdYc/edit?usp=sharing',
       },
       {
@@ -50,7 +50,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 1.4 - Git &amp; HTTP',
+        title: '1.4 - Git &amp; HTTP',
         link: 'https://docs.google.com/presentation/d/1DnI1P4wV-ndbDW59QeDppIyBE4u-mZuAEbXLVuOgDAo/edit?usp=sharing',
       },
     ],
@@ -68,7 +68,7 @@ const content = [
       {
         class: 'slide',
         link: 'https://docs.google.com/presentation/d/1d_ym6Q5Ttibrm3MvJXEuXPcqowOi_1k71V-zHa4WSQ4/edit?usp=sharing',
-        title: '&#127910; 2.1 - CSS Basics',
+        title: '2.1 - CSS Basics',
       },
       {
         class: 'demo',
@@ -88,7 +88,7 @@ const content = [
       {
         class: 'slide',
         link: 'https://docs.google.com/presentation/d/1fRxpx68_1movOy8br38NF7laM4nHUFB-4kYhxfnEQNI/edit?usp=sharing',
-        title: '&#127910; 2.2 - CSS Responsive',
+        title: '2.2 - CSS Responsive',
       },
       {
         class: 'demo',
@@ -113,7 +113,7 @@ const content = [
       {
         class: 'slide',
         link: 'https://docs.google.com/presentation/d/1fRnXFrAKR7MV4FoMb3QUPv-1HkhxBfYqs6aw_RC5NQk/edit?usp=sharing',
-        title: '&#127910; 2.3 - Design',
+        title: '2.3 - Design',
       },
       {
         class: 'exercise',
@@ -123,7 +123,7 @@ const content = [
       {
         class: 'slide',
         link: 'https://docs.google.com/presentation/d/1eBO_uZJRcimGnqq72w3H1DFdHx1paMlFqty395eUq2M/edit?usp=sharing',
-        title: '&#127910; 2.4 - The good, bad &amp; ugly',
+        title: '2.4 - The good, bad &amp; ugly',
       },
     ],
   },
@@ -139,7 +139,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 3.1 - JS Basics',
+        title: '3.1 - JS Basics',
         link: 'https://docs.google.com/presentation/d/15Ni_4sM0qNBkelF7FGdH6JntZQiH5WY1QSDq3PKvR7U/edit?usp=sharing',
       },
       {
@@ -149,7 +149,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 3.2 - JS Basics &amp; Promises',
+        title: '3.2 - JS Basics &amp; Promises',
         link: 'https://docs.google.com/presentation/d/1jM_oat95mWhjVev-1RbEpaYnhQrT8nwQ3lSWJu9dElQ/edit?usp=sharing',
       },
       {
@@ -164,7 +164,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 3.3 - DOM',
+        title: '3.3 - DOM',
         link: 'https://docs.google.com/presentation/d/1D6K024GRaRQl8dMQqe4VMtN4RBUZgto1_mYhkeIT7mA/edit?usp=sharing',
       },
       {
@@ -174,7 +174,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 3.4 - Fetch, CORS, &amp; HTTP',
+        title: '3.4 - Fetch, CORS, &amp; HTTP',
         link: 'https://docs.google.com/presentation/d/1vnu2DFrSeu9aFzRQjyS9aIvoORTyeR_AEJ-E0RsQj6Q/edit?usp=sharing',
       },
       {
@@ -184,7 +184,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 3.5 - Debugging &amp; Performance',
+        title: '3.5 - Debugging &amp; Performance',
         link: 'https://docs.google.com/presentation/d/12TwA11lXpqqUudhnxqchmANkL33jKH0zNJIVxtsYA18/edit?usp=sharing',
       },
       {
@@ -199,7 +199,7 @@ const content = [
       },
       {
         class: 'slide',
-        title: '&#127910; 3.6 - REST, GraphQL, Midterm Review',
+        title: '3.6 - REST, GraphQL, Midterm Review',
         link: 'https://docs.google.com/presentation/d/1dPFbDGXnaU9EfhK-P_FKNpeB-Jlvxq0akKY0X8R5ES0/edit?usp=sharing',
       },
       {
