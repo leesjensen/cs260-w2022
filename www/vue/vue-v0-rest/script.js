6ed03060e0bd8ac2811e6549231384d8c760642f
leesjensen <leesjensen@gmail.com>
2022-02-12 16:08:13 -0700

    Upgraded city endpoint to HTTPS

--- www/vue/vue-v0-rest/script.js
+++ www/vue/vue-v0-rest/script.js
@@ -1,8 +1,8 @@
 var app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     cities: [],
-    prefix: "",
+    prefix: '',
   },
   methods: {
     fetchREST() {
@@ -10,22 +10,22 @@ var app = new Vue({
       if (query.length > 0) {
         query = query[0].toUpperCase() + query.slice(1).toLowerCase();
       } else {
-        query = "*";
+        query = '*';
       }
-      var url = `http://bioresearch.byu.edu/cs260/jquery/getcity.cgi?q=${query}`;
+      var url = `https://cs260.click/api/city?q=${query}`;
       fetch(url)
         .then((data) => {
           return data.json();
         })
         .then((citylist) => {
-          console.log("CityList");
+          console.log('CityList');
           console.log(citylist);
           this.cities = [];
           for (let i = 0; i < citylist.length; i++) {
             console.log(citylist[i].city);
             this.cities.push({ name: citylist[i].city });
           }
-          console.log("Got Citylist");
+          console.log('Got Citylist');
         });
     },
   },
