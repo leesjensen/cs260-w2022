11946ad8ef50797c4f85319c98385a159432c3e3
Lee <lee.jensen@agilix.com>
2022-02-23 14:44:42 -0700

    enhance: vue cli demo

--- www/vue/vue-cli-demo/src/App.vue
+++ www/vue/vue-cli-demo/src/App.vue
@@ -1,34 +1,48 @@
 <template>
   <div id="app">
-    <CenteredDiv class="fill-screen" @click="clicked">I am simple.</CenteredDiv>
+    <centered-div :class="['fill-view', {'highlight':clicked}]" @click="toggleClicked">I am simple.</centered-div>
   </div>
 </template>
 
 <script>
-import CenteredDiv from './components/CenteredDiv.vue';
+import CenteredDiv from './components/CenteredDiv';
 
 export default {
   name: 'App',
   components: {
     CenteredDiv,
   },
+  data: function() {
+    return {
+      clicked: false,
+    }
+  },
   methods: {
-    clicked() {
-      console.log('clicked');
+    toggleClicked() {
+      this.clicked = !this.clicked;
     },
   },
 };
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
-.fill-screen {
+.fill-view {
   font-size: 3em;
   height: 100vh;
   color: whitesmoke;
   background: rgb(54, 87, 105);
+}
+
+.highlight {
+  color: orange;
 }
 </style>
--- www/vue/vue-cli-demo/src/components/CenteredDiv.vue
+++ www/vue/vue-cli-demo/src/components/CenteredDiv.vue
@@ -1,5 +1,5 @@
 <template>
-  <div><slot></slot></div>
+  <div v-on="$listeners"><slot></slot></div>
 </template>
 
 <script>
