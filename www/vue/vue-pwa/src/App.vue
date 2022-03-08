4a4491b9f4185f563cc164c22e24de66d5d16fa7
Lee <lee.jensen@agilix.com>
2022-03-07 17:42:32 -0700

    enhance: pwa display image

--- www/vue/vue-pwa/src/App.vue
+++ www/vue/vue-pwa/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <input
+      type="file"
+      accept="image/x-png,image/jpeg,image/gif"
+      v-on:change="upload"
+    />
+    <img src="" id="preview" />
+
     <BouncyBall />
   </div>
 </template>
@@ -11,6 +18,38 @@ export default {
   name: 'App',
   components: {
     BouncyBall,
+  },
+  methods: {
+    upload() {
+      const filePicker = document.querySelector('input');
+      console.log(filePicker);
+
+      if (!filePicker || !filePicker.files || filePicker.files.length <= 0) {
+        console.log('No file selected.');
+        return;
+      }
+      const myFile = filePicker.files[0];
+      console.log(myFile);
+
+      const fileReader = new FileReader();
+      if (fileReader && myFile) {
+        fileReader.readAsDataURL(myFile);
+        fileReader.onload = () => {
+          console.log(fileReader.result);
+
+          const img = document.querySelector('img');
+          img.setAttribute('src', fileReader.result);
+        };
+
+        fileReader.onerror = (error) => {
+          console.log(error);
+        };
+      } else {
+        console.log('No file provided');
+      }
+
+      console.log('blah');
+    },
   },
 };
 </script>
--- www/vue/vue-pwa/vue.config.js
+++ www/vue/vue-pwa/vue.config.js
@@ -1,19 +1,22 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  configureWebpack: {
+    devtool: 'source-map',
+  },
   transpileDependencies: true,
   pwa: {
-    name: "Bouncy Red Ball",
-    short_name: "Bouncy",
-    themeColor: "#00FF00",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
+    name: 'Bouncy Red Ball',
+    short_name: 'Bouncy',
+    themeColor: '#00FF00',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
     icons: {
-      favicon32: "img/icons/favicon-32x32.png",
-      favicon16: "img/icons/favicon-16x16.png",
-      appleTouchIcon: "img/icons/apple-touch-icon.png",
-      msTileImage: "img/icons/msapplication-icon-150x150.png",
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      msTileImage: 'img/icons/msapplication-icon-150x150.png',
     },
-    display: "standalone",
+    display: 'standalone',
   },
 });
