e91b0b279d356eabcf2e84933ca5686d205bacd4
Lee <lee.jensen@agilix.com>
2022-03-18 17:10:41 -0600

    fix: lab3b to be relative paths

--- www/vue/vue-lab3b-grocery/src/router/index.js
+++ www/vue/vue-lab3b-grocery/src/router/index.js
@@ -1,32 +1,32 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Browse from "../views/Browse.vue";
-import Cart from "../views/Cart.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Browse from '../views/Browse.vue';
+import Cart from '../views/Cart.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/browse",
-    name: "Browse",
+    path: '/browse',
+    name: 'Browse',
     component: Browse,
   },
   {
-    path: "/cart",
-    name: "Cart",
+    path: '/cart',
+    name: 'Cart',
     component: Cart,
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+  mode: 'history',
+  base: window.location.pathname.replace(/\/+$/, ''),
   routes,
 });
 
