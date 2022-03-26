3a0de453f5a5aebd58b602fbd6546d2cf6a9ac9c
leesjensen <leesjensen@gmail.com>
2022-03-26 14:46:39 -0600

    add: simple websocket demo

--- www/node/node-websocket/chat/chat.js
+++ www/node/node-websocket/chat/chat.js
@@ -1,16 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('./public'));
-
-app.get('/dynamic', (req, res, next) => {
-  res.send('<h1>Hello from dynamic content!<h1>');
-});
-
-app.use(express.json());
-app.post('/user/:id', function (req, res) {
-  res.send({ responseName: req.body.name });
-});
+app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(5090, () => {
   console.log(`Listening on port 5090`);
