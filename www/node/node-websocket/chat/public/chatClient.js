053d8b27a4ec6f505f1d56a86ce435eda1159c70
leesjensen <leesjensen@gmail.com>
2022-03-26 16:52:37 -0600

    cleanup: chat websocket

--- www/node/node-websocket/chat/public/chatClient.js
+++ www/node/node-websocket/chat/public/chatClient.js
@@ -2,12 +2,12 @@ const socket = new WebSocket('ws://localhost:8080');
 
 socket.onopen = (event) => {
   appendMsg('system', 'websocket', 'connected');
-  enableChat(true);
 };
 
 socket.onclose = (event) => {
   appendMsg('system', 'websocket', 'disconnected');
-  enableChat(false);
+  document.querySelector('#name-controls').disabled = true;
+  document.querySelector('#chat-controls').disabled = true;
 };
 
 socket.onmessage = (event) => {
@@ -36,6 +36,7 @@ function getMyName() {
   return myName.replace(/\s/g, '_');
 }
 
+// Enable send if text to send
 const input = document.querySelector('#new-msg');
 input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
@@ -43,16 +44,9 @@ input.addEventListener('keydown', (e) => {
   }
 });
 
+// Disable chat if no name provided
 const chatControls = document.querySelector('#chat-controls');
 const myName = document.querySelector('#my-name');
 myName.addEventListener('keyup', (e) => {
-  console.log(myName.value);
   chatControls.disabled = myName.value === '';
 });
-
-function enableChat(enabled) {
-  document.querySelector('#name-controls').disabled = !enabled;
-  document.querySelector('#chat-controls').disabled = true;
-  document.querySelector('#my-name').value = '';
-  document.querySelector('#new-msg').value = '';
-}
